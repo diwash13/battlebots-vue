68433283c329d03690c39a7ad7e4e43fb95f7f0d
Diwash Sigdel <priyankasharma@Priyankas-MacBook-Air.local>
2019-03-26 21:42:39 -0600

    first commit

--- src/App.vue
+++ src/App.vue
@@ -1,17 +1,110 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <nav class='nav'>
+    <button class='title' @click="toggle" >Create Bot</button>
+    <h1>Battle Bots</h1>
+    <button class='title' @click="toggle" >Collection</button>
+    </nav>
+    <section class="container" v-if='showForm' >
+      <input  class='input' type="text" placeholder="Bot Name*" v-model="bot">
+      <br>
+      <input class='input' type="text" placeholder="Attack Value*" v-model="attack">
+      <button @click="increment">+</button>
+      <button @click="decrement">-</button>
+      <br>
+      <input class='input' type="text" placeholder="Health Value*" v-model="health">
+      <button @click="inc">+</button>
+      <button @click="dec">-</button>
+      <br>
+      <button @click='createBot'>Submit</button>
+      <button>Clear</button>
+      
+    </section>
+    <section v-else>
+      <h3>Bot 1</h3>
+      <h3 v-if="selectedBots[0]">Bot #1: {{selectedBots[0].bot}}</h3>
+      <h3 v-if="selectedBots[1]">Bot #2: {{selectedBots[1].bot}}</h3>
+      <button @click='battle' >Battle</button>
+      <button @click="clearSelected">Clear</button>
+      <hr>
+      <bot v-for="(botObj, i) in allBots" 
+        :key='i' 
+        :index = 'i' 
+        :bot-obj='botObj' 
+        :retire-bot = 'retireBot'
+        :selectBot = 'selectBot' 
+        />
+      <!-- <bot v-for='(bot, i) in allBots' :key='i' :index='i' :retireBot='retireBot' /> -->
+    </section>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Bot from './components/Bot'
 export default {
   name: 'app',
+  data: function() {
+    return {
+      showForm: true,
+      bot: '',
+      attack: '',
+      health: '',
+      allBots: [
+        {bot: 'Frank', attack: 30, health: 20},
+        {bot: 'Harry', attack: 40, health: 10},
+        {bot: 'Pearl', attack: 10, health: 40}
+        ],
+      selectedBots: []
+    }
+  },
+  methods: {
+    increment: function() {
+      this.attack++
+    },
+    decrement: function() {
+      this.attack--
+    },
+    inc: function() {
+      this.health++
+    },
+    dec: function() {
+      this.health--
+    },
+    toggle: function() {
+      this.showForm = !this.showForm
+    },
+    createBot: function() {
+      const newBot = {bot: this.bot, attack: this.attack, health: this.health}
+      this.allBots.push(newBot)
+      this.bot = ''
+      this.attack = ''
+      this.health = ''
+      this.toggle()
+  },
+  retireBot: function (i) {
+      this.allBots = this.allBots.filter((bot, index) => index !== i)
+    },
+    selectBot: function(newBot) {
+      if (this.selectedBots.length < 2) {
+        this.selectedBots.push(newBot);
+      } else {
+        alert('2 Bots Have Been Selected');
+      }
+  },
+  clearSelected: function() {
+    this.selectedBots = []
+  },
+  battle() {
+      if (this.selectedBots[0].attack > this.selectedBots[1].attack) {
+        alert(`${this.selectedBots[0].bot} wins!`);
+      } else {
+        alert(`${this.selectedBots[1].bot} wins!`);
+      }
+      this.selectedBots = [];
+    }
+  },
   components: {
-    HelloWorld
+    Bot: Bot
   }
 }
 </script>
@@ -24,5 +117,38 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  background-color: #00E498;
+  height: 100vh;
+}
+.nav {
+  display: flex;
+  justify-content: space-evenly;
+  color: white;
+  background-color: #00336D;
+  height: 100px;
+  width: 100vw;
+}
+.title {
+    font-size: 20px!important;
+    font-weight: 500;
+    line-height: 1!important;
+    letter-spacing: .02em!important;
+    background-color: #00336D;
+    color: white;
+}
+.container {
+    height: 300px;
+    width: 500px;
+    margin-top: 50px;
+    margin-left: 400px;
+    background-color: white;
+
+}
+.input {
+    margin-top: 50px;
+    height: 30px;
+    width: 80%;
+    font-family: -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    font-size: 20px;
 }
 </style>
---
+++ src/components/Bot.vue
@@ -0,0 +1,16 @@
+<template>
+    <div>
+        <h2>{{botObj.bot}}</h2>
+        <p>Attack:{{botObj.attack}}</p>
+        <p>Health:{{botObj.health}}</p>
+        <button @click='selectBot()'>Select</button>
+        <button @click='retireBot(index)'>Retire</button>
+        <hr>
+    </div>
+</template>
+
+<script>
+export default {
+    props: ['botObj', 'index', 'retireBot', 'selectBot']
+}
+</script>
